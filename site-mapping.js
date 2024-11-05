4752b9184e57a0a0057d3fc1137ceab191b4a198
jman1593 <josh@pettits.net>
2024-11-05 16:52:31 -0700

    add better connectivity

---
+++ site-mapping.js
@@ -0,0 +1,12 @@
+export const siteMapping = {
+    "mathcord.netlify.app": "https://mathcord2.netlify.app",
+    "mathcord2.netlify.app": "https://mathcord2.netlify.app",
+    "strongdog.netlify.app": "https://strongdog2.netlify.app",
+    "strongdog2.netlify.app": "https://strongdog2.netlify.app",
+    "stroongdog.netlify.app": "https://stroongdog2.netlify.app",
+    "jman1593.github.io": "https://jman1593.github.io/strongdog2",
+    "strongdog-1.vercel.app": "https://strongdog2.vercel.app",
+    "strongdog.onrender.com": "https://strongdog2.onrender.com",
+    "127.0.0.1": "http://127.0.0.1:8080/strongdog2/",
+  };
+  
