b81c18bb0409379c265a05cf9b0f086483019fd8
jman1593 <josh@pettits.net>
2024-11-08 14:51:08 -0700

    remove

--- cards-data.js
+++ cards-data.js
@@ -1,6 +1,5 @@
 export default [
   	{ href: './html/three goblets/index.html', imgSrc: 'three goblets.png', name: 'three goblets' , 'page': 2},
-  	{ href: './html/the final earth/index.html', imgSrc: 'the final earth.jpg', name: 'the final earth' , 'page': 2},
   	{ href: './html/super pickleball adventure/index.html', imgSrc: 'super pickleball adventure.png', name: 'super pickleball adventure' , 'page': 2},
   	{ href: './html/squareX/index.html', imgSrc: 'squarex.jpg', name: 'squareX' , 'page': 2},
   	{ href: './html/metrocubevania/index.html', imgSrc: 'metrocubevania.png', name: 'metrocubevania' , 'page': 2},
