c737527e5e69581da36548024179a7aa24076d44
jman1593 <josh@pettits.net>
2024-11-08 07:13:38 -0700

    add new games to testing

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,3 +1,7 @@
 export default [
+      { href: './html/block shooter frenzy/index.html', imgSrc: 'block shooter frenzy.jpg', name: 'block shooter frenzy' , 'page': 2},
+      { href: './html/block rush/index.html', imgSrc: 'block rush.png', name: 'block rush' , 'page': 2},
+      { href: './html/Blocco/index.html', imgSrc: 'Blocco.jpg', name: 'Blocco' , 'page': 2},
+      { href: './html/arrozoid/index.html', imgSrc: 'arrozoid.png', name: 'arrozoid' , 'page': 2},
 
 ];
