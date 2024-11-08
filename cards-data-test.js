c49112e0c062c71e7635998cf96745011e81000c
jman1593 <josh@pettits.net>
2024-11-07 21:03:27 -0700

    add testing panel

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,5 +1,3 @@
 export default [
-    { href: './html/three goblets/index.html', imgSrc: 'three goblets.png', name: 'three goblets' , 'page': 2},
-    { href: './html/the final earth/index.html', imgSrc: 'the final earth.jpg', name: 'the final earth' , 'page': 2},
-    { href: './html/super pickleball adventure/index.html', imgSrc: 'super pickleball adventure.png', name: 'super pickleball adventure' , 'page': 2},
+
 ];
