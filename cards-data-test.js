152e8c5e28bcdfdfe098c162c4014dce9195fcd1
jman1593 <josh@pettits.net>
2024-11-12 18:19:21 -0700

    fix

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,3 +1,4 @@
 export default [
 	{ href: './html/block shooter frenzy/index.html', imgSrc: 'block shooter frenzy.jpg', name: 'block shooter frenzy' , 'page': 2},
 	{ href: './html/Blocco/index.html', imgSrc: 'Blocco.jpg', name: 'Blocco' , 'page': 2},
+];
