7a713ff38ee055ac062869a8a3c202b79a13d2b2
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-11-21 08:13:24 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/pacman remake/index.html', imgSrc: 'pacman remake.png', name: 'pacman remake' , 'page': 3},
   { href: './html/cannon basketball 3/index.html', imgSrc: 'cannon basketball 3.jpg', name: 'cannon basketball 3' , 'page': 2},
   { href: './html/trafficmania/index.html', imgSrc: 'trafficmania.jpg', name: 'trafficmania' , 'page': 2},
 	{ href: './html/red ball 4/index.html', imgSrc: 'red ball 4.jpg', name: 'red ball 4' , 'page': 2},
