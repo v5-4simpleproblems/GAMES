c6f8485600b6938536c713017ebc49eb0b1475b8
jman1593 <josh@pettits.net>
2024-11-22 19:54:35 -0700

    fix coco monkey

--- cards-data-test.js
+++ cards-data-test.js
@@ -16,7 +16,7 @@ export default [
       { href: './html/small world cup/index.html', imgSrc: 'small world cup.jpg', name: 'small world cup' , 'page': 3},
       { href: './html/tricksplit.io/index.html', imgSrc: 'tricksplit.io.jpg', name: 'tricksplit.io' , 'page': 2},
       { href: './html/tower defense 2d/index.html', imgSrc: 'tower defense 2d.jpg', name: 'tower defense 2d' , 'page': 2},
-      { href: './html/coco monkey/index.html', imgSrc: 'coco monkey.jpg', name: 'coco monkey' , 'page': 2},
+      { href: './html/coco monkey/game/index.html', imgSrc: 'coco monkey.jpg', name: 'coco monkey' , 'page': 2},
       { href: './html/cities of babylon/index.html', imgSrc: 'cities of babylon.jpg', name: 'cities of babylon' , 'page': 2},
       { href: './html/chicken hike/index.html', imgSrc: 'chicken hike.jpg', name: 'chicken hike' , 'page': 2},
       { href: './html/cherry on the ice cream/index.html', imgSrc: 'cherry on the ice cream.jpg', name: 'cherry on the ice cream' , 'page': 2},
