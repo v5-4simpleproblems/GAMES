bf5f493f92772bc1ab460b545fd5316f03ae7281
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-11-25 09:14:19 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/ragdoll hit/index.html', imgSrc: 'ragdoll hit.jpg', name: 'ragdoll hit' , 'page': 3},
     { href: './html/skyblock/index.html', imgSrc: 'skyblock.jpg', name: 'skyblock' , 'page': 3},
     { href: './html/cannon basketball/index.html', imgSrc: 'cannon basketball.jpg', name: 'cannon basketball' , 'page': 2},
     { href: './html/cannon basketball 2/index.html', imgSrc: 'cannon basketball 2.jpg', name: 'cannon basketball 2' , 'page': 2},
