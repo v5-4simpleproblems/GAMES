d888f8347290362fc524f69a2384fac66c3d95c0
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-02 12:49:15 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/minecraft case simulator/index.html', imgSrc: 'minecraft case simulator.jpg', name: 'minecraft case simulator' , 'page': 3},
   { href: './html/roper/index.html', imgSrc: 'roper.jpg', name: 'roper' , 'page': 3},
   { href: './html/bit planes/index.html', imgSrc: 'bit planes.jpg', name: 'bit planes' , 'page': 3},
   { href: './html/idle tree city/index.html', imgSrc: 'idle tree city.jpg', name: 'idle tree city' , 'page': 3},
