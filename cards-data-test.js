42dd0abae34f9c62e80addcc1c7ada875a90c863
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-03 10:29:43 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -27,7 +27,6 @@ export default [
       { href: './html/chicken hike/index.html', imgSrc: 'chicken hike.jpg', name: 'chicken hike' , 'page': 2},
       { href: './html/cherry on the ice cream/index.html', imgSrc: 'cherry on the ice cream.jpg', name: 'cherry on the ice cream' , 'page': 2},
       { href: './html/beavus/index.html', imgSrc: 'beavus.jpg', name: 'beavus' , 'page': 2},
-      { href: './html/b-side/index.html', imgSrc: 'b-side.jpg', name: 'b-side' , 'page': 2},
       { href: './html/assembloid/index.html', imgSrc: 'assembloid.png', name: 'assembloid' , 'page': 2},
       { href: './html/ARROW/index.html', imgSrc: 'ARROW.jpg', name: 'ARROW' , 'page': 2},
       { href: './html/antiban/index.html', imgSrc: 'antiban.png', name: 'antiban' , 'page': 2},
