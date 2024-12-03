2ce3075115f039b9fc973d94eb189cee1226e812
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-03 10:29:43 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/b-side/index.html', imgSrc: 'b-side.jpg', name: 'b-side' , 'page': 2},
   { href: './html/baw/index.html', imgSrc: 'baw.jpg', name: 'baw' , 'page': 2},
   { href: './html/pixel gun apocalypse 2/index.html', imgSrc: 'pixel gun apocalypse 2.jpg', name: 'pixel gun apocalypse 2' , 'page': 3},
   { href: './html/minecraft case simulator/index.html', imgSrc: 'minecraft case simulator.jpg', name: 'minecraft case simulator' , 'page': 3},
