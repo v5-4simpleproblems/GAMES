36dfb5bfa428ec4c03a25d0d5220fb79e2a44ca2
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-06 11:31:49 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -8,7 +8,6 @@ export default [
       { href: './html/stickbattle.lol/index.html', imgSrc: 'stickbattle.lol.jpg', name: 'stickbattle.lol' , 'page': 3},
       { href: './html/only up/index.html', imgSrc: 'only up.jpg', name: 'only up' , 'page': 3},
       { href: './html/snay.io/index.html', imgSrc: 'snay.io.jpg', name: 'snay.io' , 'page': 3},
-      { href: './html/small world cup/index.html', imgSrc: 'small world cup.jpg', name: 'small world cup' , 'page': 3},
       { href: './html/tricksplit.io/index.html', imgSrc: 'tricksplit.io.jpg', name: 'tricksplit.io' , 'page': 2},
       { href: './html/tower defense 2d/index.html', imgSrc: 'tower defense 2d.jpg', name: 'tower defense 2d' , 'page': 2},
       { href: './html/coco monkey/game/index.html', imgSrc: 'coco monkey.jpg', name: 'coco monkey' , 'page': 2},
