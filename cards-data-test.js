171819c6241e7cae56480cd23b23815cf9db2d88
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-08 22:00:47 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,3 +1,2 @@
 export default [
-      { href: './html/big flappy tower tiny square/index.html', imgSrc: 'big flappy tower tiny square.jpg', name: 'big flappy tower tiny square' , 'page': 3, 'id': 716},
 ]
