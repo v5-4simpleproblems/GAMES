dbe40b38bfc45db7eef16b09e77c37d58842e81c
jman1593 <josh@pettits.net>
2024-12-10 16:58:54 -0700

    add more games to test menu

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,2 +1,8 @@
 export default [
+      { href: './html/NGU idle/index.html', imgSrc: 'NGU idle.jpg', name: 'NGU idle' , 'page': 3, 'id': 717 },
+      { href: './html/football strike/index.html', imgSrc: 'football strike.jpg', name: 'football strike' , 'page': 3, 'id': 718 },
+      { href: './html/fishing and lines/index.html', imgSrc: 'fishing and lines.jpg', name: 'fishing and lines' , 'page': 3, 'id': 719 },
+      { href: './html/fightz.io/index.html', imgSrc: 'fightz.io.jpg', name: 'fightz.io' , 'page': 3, 'id': 720 },
+      { href: './html/endless tunnel/index.html', imgSrc: 'endless tunnel.jpg', name: 'endless tunnel' , 'page': 3, 'id': 721 },
+      { href: './html/bottle flip/index.html', imgSrc: 'bottle flip.png', name: 'bottle flip' , 'page': 3, 'id': 722 },
 ]
