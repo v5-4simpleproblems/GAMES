f7bdff2b3e7a4358e971b7f4ba0a313404b4d4e8
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-13 08:57:30 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,6 +1,5 @@
 export default [
       { href: './html/NGU idle/index.html', imgSrc: 'NGU idle.jpg', name: 'NGU idle' , 'page': 3, 'id': 717 },
-      { href: './html/football strike/index.html', imgSrc: 'football strike.jpg', name: 'football strike' , 'page': 3, 'id': 718 },
       { href: './html/fishing and lines/index.html', imgSrc: 'fishing and lines.jpg', name: 'fishing and lines' , 'page': 3, 'id': 719 },
       { href: './html/fightz.io/index.html', imgSrc: 'fightz.io.jpg', name: 'fightz.io' , 'page': 3, 'id': 720 },
 ]
