050eeef616df68c0765545b65329eafa72ec0b76
jman1593 <josh@pettits.net>
2024-12-13 20:22:37 -0700

    add new games to testing

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,7 @@
 export default [
+      { href: './swf/toxic/index.html', imgSrc: 'toxic.jpg', name: 'toxic' , 'page': 3, 'id': 723 },
+      { href: './swf/toxic 2/index.html', imgSrc: 'toxic 2.jpg', name: 'toxic 2' , 'page': 3, 'id': 722 },
+      { href: './swf/tanked up/index.html', imgSrc: 'tanked up.jpg', name: 'tanked up' , 'page': 3, 'id': 721 },
       { href: './html/NGU idle/index.html', imgSrc: 'NGU idle.jpg', name: 'NGU idle' , 'page': 3, 'id': 717 },
       { href: './html/fishing and lines/index.html', imgSrc: 'fishing and lines.jpg', name: 'fishing and lines' , 'page': 3, 'id': 719 },
       { href: './html/fightz.io/index.html', imgSrc: 'fightz.io.jpg', name: 'fightz.io' , 'page': 3, 'id': 720 },
