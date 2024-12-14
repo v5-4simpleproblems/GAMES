3d0242c2750cc1a6f01324b415ec317ffbc2386b
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-14 14:49:44 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,5 +1,4 @@
 export default [
-      { href: './swf/toxic 2/index.html', imgSrc: 'toxic 2.jpg', name: 'toxic 2' , 'page': 3, 'id': 722 },
       { href: './swf/tanked up/index.html', imgSrc: 'tanked up.jpg', name: 'tanked up' , 'page': 3, 'id': 721 },
       { href: './html/NGU idle/index.html', imgSrc: 'NGU idle.jpg', name: 'NGU idle' , 'page': 3, 'id': 717 },
       { href: './html/fishing and lines/index.html', imgSrc: 'fishing and lines.jpg', name: 'fishing and lines' , 'page': 3, 'id': 719 },
