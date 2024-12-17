aba48cb3ea5670d4ad40d9878d620561588002a8
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-17 08:37:12 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './swf/tanked up/index.html', imgSrc: 'tanked up.jpg', name: 'tanked up' , 'page': 3, 'id': 721 },
   { href: './html/footballbros/index.html', imgSrc: 'footballbros.jpg', name: 'footballbros' , 'page': 3, 'id': 726 },
   { href: './html/backgammon/index.html', imgSrc: 'backgammon.jpg', name: 'backgammon' , 'page': 3, 'id': 728 },
 	{ href: './html/nzp/index.html', imgSrc: 'nzp.jpg', name: 'nzp' , 'page': 3, 'id': 725 },
