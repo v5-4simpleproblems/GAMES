9a07e9585d03026dc986ddea4caff010bb0b3376
jman1593 <josh@pettits.net>
2024-12-18 15:22:57 -0700

    add id fixes

--- cards-data.js
+++ cards-data.js
@@ -1,5 +1,5 @@
 export default [
-	{ href: './swf/tanked up/index.html', imgSrc: 'tanked up.jpg', name: 'tanked up' , 'page': 3, 'id': 721, tags: [''] },
+	{ href: './swf/tanked up/index.html', imgSrc: 'tanked up.jpg', name: 'tanked up' , 'page': 3, 'id': 727, tags: [''] },
 	{ href: './html/footballbros/index.html', imgSrc: 'footballbros.jpg', name: 'footballbros' , 'page': 3, 'id': 726, tags: [''] },
 	{ href: './html/backgammon/index.html', imgSrc: 'backgammon.jpg', name: 'backgammon' , 'page': 3, 'id': 728, tags: [''] },
   { href: './html/nzp/index.html', imgSrc: 'nzp.jpg', name: 'nzp' , 'page': 3, 'id': 725, tags: [''] },
