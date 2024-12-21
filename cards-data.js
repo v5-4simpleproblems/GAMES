6812929745b3fc3ac3675e011b69b2657ae506c0
jman1593 <josh@pettits.net>
2024-12-20 22:35:05 -0700

    fix

--- cards-data.js
+++ cards-data.js
@@ -1,5 +1,4 @@
 export default [
-  { href: './html/block blast/index.html', imgSrc: 'block blast.jpg', name: 'block blast' , 'page': 3, 'id': 730 },
   { href: './html/fishing and lines/index.html', imgSrc: 'fishing and lines.jpg', name: 'fishing and lines' , 'page': 3, 'id': 719 },
   { href: './html/NGU idle/index.html', imgSrc: 'NGU idle.jpg', name: 'NGU idle' , 'page': 3, 'id': 717 },
   { href: './html/golf orbit/index.html', imgSrc: 'golf orbit.jpg', name: 'golf orbit' , 'page': 3, 'id': 729, tags: [''] },
