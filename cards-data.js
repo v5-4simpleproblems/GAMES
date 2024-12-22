3dd6bff5240bd976e51fb19b05d9b0cac01aed71
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-22 14:17:10 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/Red vs Blue/index.html', imgSrc: 'Red vs Blue.png', name: 'Red vs Blue' , 'page': 3, 'id': 740 },
   { href: './html/block blast/index.html', imgSrc: 'block blast.jpg', name: 'block blast' , 'page': 3, 'id': 730 },
   { href: './html/fishing and lines/index.html', imgSrc: 'fishing and lines.jpg', name: 'fishing and lines' , 'page': 3, 'id': 719 },
   { href: './html/NGU idle/index.html', imgSrc: 'NGU idle.jpg', name: 'NGU idle' , 'page': 3, 'id': 717 },
