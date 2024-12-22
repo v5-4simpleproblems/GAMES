d407de4ae55a3282ba1a3681144114b23274a6e7
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-22 14:18:59 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,3 @@
 export default [
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
-      { href: './html/amazing strange rope police/index.html', imgSrc: 'amazing strange rope police.jpg', name: 'amazing strange rope police' , 'page': 3, 'id': 733 },
 ]
