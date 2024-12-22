f9a7d02350a98d8dbfb1d7067d66a8ff6567ddf8
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-12-22 14:17:32 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,7 +1,6 @@
 export default [
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
       { href: './html/money movers 2/index.html', imgSrc: 'money movers 2.jpg', name: 'money movers 2' , 'page': 3, 'id': 737 },
-      { href: './html/money movers/index.html', imgSrc: 'money movers.jpg', name: 'money movers' , 'page': 3, 'id': 736 },
       { href: './html/Mirror Madness/index.html', imgSrc: 'Mirror Madness.png', name: 'Mirror Madness' , 'page': 3, 'id': 735 },
       { href: './html/Death Run/index.html', imgSrc: 'Death Run.png', name: 'Death Run' , 'page': 3, 'id': 734 },
       { href: './html/amazing strange rope police/index.html', imgSrc: 'amazing strange rope police.jpg', name: 'amazing strange rope police' , 'page': 3, 'id': 733 },
