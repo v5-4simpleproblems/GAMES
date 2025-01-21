75e26692a042f6d20bc321e381396dbe4f3d3b85
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-01-21 10:23:37 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,3 @@
 export default [
-      { href: './html/Dinogen arena/index.html', imgSrc: 'Dinogen arena.jpg', name: 'Dinogen arena' , 'page': 3, 'id': 741 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
 ]
