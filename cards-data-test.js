54835acbf7eb2955e03c1f6ace4a404c3bdae7d0
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-01-21 10:23:29 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,5 +1,4 @@
 export default [
-      { href: './html/five nights at winstons/index.html', imgSrc: 'five nights at winstons.jpg', name: 'five nights at winstons' , 'page': 3, 'id': 742 },
       { href: './html/Dinogen arena/index.html', imgSrc: 'Dinogen arena.jpg', name: 'Dinogen arena' , 'page': 3, 'id': 741 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
 ]
