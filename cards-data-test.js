16198758131f7776961cf24b3d876d1db833604b
jman1593 <josh@pettits.net>
2025-02-02 14:56:08 -0700

    add smash karts for testing

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,5 @@
 export default [
+      { href: './html/smashkarts/index.html', imgSrc: 'smashkarts.jpg', name: 'smashkarts' , 'page': 3, 'id': 752 },
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
       { href: './html/spotify/index.html', imgSrc: 'spotify.png', name: 'Spotify' , 'page': 3, 'id': 743 },
