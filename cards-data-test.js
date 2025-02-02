e0a690c3e657fe2507f2dcab5542a5b1a5573521
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-02 14:16:48 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -2,7 +2,6 @@ export default [
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
       { href: './html/taming.io/index.html', imgSrc: 'taming.io.jpg', name: 'taming.io' , 'page': 3, 'id': 750 },
       { href: './html/roblox/index.html', imgSrc: 'roblox.jpg', name: 'roblox' , 'page': 3, 'id': 749 },
-      { href: './html/new 1v1.lol/index.html', imgSrc: '1v1.lol.jpg', name: 'new 1v1.lol' , 'page': 3, 'id': 748 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
       { href: './html/spotify/index.html', imgSrc: 'spotify.png', name: 'Spotify' , 'page': 3, 'id': 743 },
 ]
