f75a2e2ec150005243b2e0e1a8bb279bf9c6cc75
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-02 11:32:54 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -5,7 +5,6 @@ export default [
       { href: './html/new 1v1.lol/index.html', imgSrc: '1v1.lol.jpg', name: 'new 1v1.lol' , 'page': 3, 'id': 748 },
       { href: './html/human or not/index.html', imgSrc: 'human or not.png', name: 'human or not' , 'page': 3, 'id': 746 },
       { href: './html/discord/game/index.html', imgSrc: 'discord.png', name: 'discord' , 'page': 3, 'id': 745 },
-      { href: './html/amazon/game/index.html', imgSrc: 'amazon.png', name: 'amazon' , 'page': 3, 'id': 744 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
       { href: './html/spotify/index.html', imgSrc: 'spotify.png', name: 'Spotify' , 'page': 3, 'id': 743 },
 ]
