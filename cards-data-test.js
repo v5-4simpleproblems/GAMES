29b1bf37c91b1b69f25ef71c3cd9b1d454ce1137
jman1593 <josh@pettits.net>
2025-02-02 21:11:22 -0700

    add death by ai

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,5 @@
 export default [
+      { href: './html/death by ai/index.html', imgSrc: 'death by ai.jpg', name: 'death by ai' , 'page': 3, 'id': 754 },
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
       { href: './html/spotify/index.html', imgSrc: 'spotify.png', name: 'Spotify' , 'page': 3, 'id': 743 },
