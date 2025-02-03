8044b30997c6b49ddbdb18ff48b10577413acbc2
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-02 21:37:16 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,5 +1,4 @@
 export default [
-      { href: './html/kour.io/index.html', imgSrc: 'kour.io.jpg', name: 'kour.io' , 'page': 3, 'id': 758 },
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
       { href: './html/spotify/index.html', imgSrc: 'spotify.png', name: 'Spotify' , 'page': 3, 'id': 743 },
