79cfe56f14606b184bdad4b8392660d7702aeb25
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-02 18:41:14 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/narwhale.io/index.html', imgSrc: 'narwhale.io.png', name: 'narwhale.io' , 'page': 3, 'id': 753 },
   { href: './html/smashkarts/index.html', imgSrc: 'smashkarts.jpg', name: 'smashkarts' , 'page': 3, 'id': 752 },
   { href: './html/roblox/index.html', imgSrc: 'roblox.jpg', name: 'roblox' , 'page': 3, 'id': 749 },
   { href: './html/taming.io/index.html', imgSrc: 'taming.io.jpg', name: 'taming.io' , 'page': 3, 'id': 750 },
