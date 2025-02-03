6516f90385e846a8640b69dbe607b49e3ababae2
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-02 21:36:48 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,7 +1,6 @@
 export default [
       { href: './html/thats not my neighbor/index.html', imgSrc: 'thats not my neighbor.jpg', name: 'thats not my neighbor' , 'page': 3, 'id': 759 },
       { href: './html/kour.io/index.html', imgSrc: 'kour.io.jpg', name: 'kour.io' , 'page': 3, 'id': 758 },
-      { href: './html/buckshot roulette/index.html', imgSrc: 'buckshot roulette.jpg', name: 'buckshot roulette' , 'page': 3, 'id': 757 },
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
       { href: './html/spotify/index.html', imgSrc: 'spotify.png', name: 'Spotify' , 'page': 3, 'id': 743 },
