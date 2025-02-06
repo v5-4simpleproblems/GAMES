ea155923b92bd0a5d9e6f2ea5b5756432a72ecbd
jman1593 <josh@pettits.net>
2025-02-06 16:24:00 -0700

    add new territorial.io

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,5 @@
 export default [
+      { href: './html/new territorial.io/index.html', imgSrc: 'new territorial.io.jpg', name: 'new territorial.io' , 'page': 3, 'id': 772 },
       { href: './swf/learn to fly 3/index.html', imgSrc: 'learn to fly 3.jpg', name: 'learn to fly 3' , 'page': 3, 'id': 771 },
       { href: './swf/learn to fly 2/index.html', imgSrc: 'learn to fly 2.jpg', name: 'learn to fly 2' , 'page': 3, 'id': 770 },
       { href: './swf/drunk guy/index.html', imgSrc: 'drunk guy.jpg', name: 'drunk guy' , 'page': 3, 'id': 769 },
