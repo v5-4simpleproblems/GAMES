111d25916e0a5acbf703992846133308167637a2
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-06 10:07:41 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,6 +1,5 @@
 export default [
       { href: './swf/learn to fly 3/index.html', imgSrc: 'learn to fly 3.jpg', name: 'learn to fly 3' , 'page': 3, 'id': 771 },
-      { href: './swf/learn to fly 2/index.html', imgSrc: 'learn to fly 2.jpg', name: 'learn to fly 2' , 'page': 3, 'id': 770 },
       { href: './html/youtube/game/index.html', imgSrc: 'youtube.png', name: 'youtube' , 'id': 761 },
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
