88ee95021f66c9d2f445c8068536abd6cc960fec
jman1593 <josh@pettits.net>
2025-02-06 16:24:41 -0700

    add

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,5 @@
 export default [
+      { href: './html/new territorial.io/index.html', imgSrc: 'new territorial.io.jpg', name: 'new territorial.io' , 'page': 3, 'id': 772 },
       { href: './html/youtube/game/index.html', imgSrc: 'youtube.png', name: 'youtube' , 'id': 761 },
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
       { href: './html/money movers 3/index.html', imgSrc: 'money movers 3.jpg', name: 'money movers 3' , 'page': 3, 'id': 738 },
