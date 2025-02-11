e0274a618a624ae54c7b23f38b11fc73f158679d
jman1593 <josh@pettits.net>
2025-02-11 16:38:45 -0700

    add

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,4 +1,5 @@
 export default [
+      { href: './html/Pokemon Emerald/index.html', imgSrc: 'Pokemon Emerald.jpg', name: 'Pokemon Emerald' , 'page': 3, 'id': 773 },
       { href: './html/new territorial.io/index.html', imgSrc: 'new territorial.io.jpg', name: 'new territorial.io' , 'page': 3, 'id': 772 },
       { href: './html/youtube/game/index.html', imgSrc: 'youtube.png', name: 'youtube' , 'id': 761 },
       { href: './html/xbox cloud gaming/index.html', imgSrc: 'xbox cloud gaming.png', name: 'xbox cloud gaming' , 'page': 3, 'id': 751 },
