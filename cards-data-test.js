bffd436ca8052c8611b46884767f7c23f07d2216
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-27 09:29:35 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,7 +1,6 @@
 export default [
       { href: './swf/strikeforce kitty last stand/index.html', imgSrc: 'strikeforce kitty last stand.jpg', name: 'strikeforce kitty last stand' , 'page': 3, 'id': 795 },
       { href: './swf/ragdoll achievement/index.html', imgSrc: 'ragdoll achievement.jpg', name: 'ragdoll achievement' , 'page': 3, 'id': 790 },
-      { href: './swf/armor mayhem/index.html', imgSrc: 'armor mayhem.jpg', name: 'armor mayhem' , 'page': 3, 'id': 781 },
       { href: './swf/arcane weapons/index.html', imgSrc: 'arcane weapons.jpg', name: 'arcane weapons' , 'page': 3, 'id': 780 },
       { href: './html/raft wars 2/index.html', imgSrc: 'raft wars 2.jpg', name: 'raft wars 2' , 'page': 3, 'id': 777 },
       { href: './html/new territorial.io/index.html', imgSrc: 'new territorial.io.jpg', name: 'new territorial.io' , 'page': 3, 'id': 772 },
