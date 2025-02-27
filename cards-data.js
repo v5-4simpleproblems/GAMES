b42a24f7355b95f0718a684017f19b7f45779649
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-02-26 18:34:44 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './swf/kids vs santa/index.html', imgSrc: 'kids vs santa.jpg', name: 'kids vs santa' , 'page': 3, 'id': 788 },
   { href: './swf/kids vs ice cream/index.html', imgSrc: 'kids vs ice cream.jpg', name: 'kids vs ice cream' , 'page': 3, 'id': 787 },
   { href: './swf/mega miner/index.html', imgSrc: 'mega miner.jpg', name: 'mega miner' , 'page': 3, 'id': 789 },
   { href: './swf/sniper assasin/index.html', imgSrc: 'sniper assasin.jpg', name: 'sniper assasin' , 'page': 3, 'id': 792 },
