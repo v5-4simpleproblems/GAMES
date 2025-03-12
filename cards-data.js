5c6bf7ad1caf5b683cecca5400bcfcf3d0d1f351
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-12 10:08:04 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/burger clicker/index.html', imgSrc: 'burger clicker.jpg', name: 'burger clicker' , 'page': 3, 'id': 814 },
   { href: './html/bouncy woods/index.html', imgSrc: 'bouncy woods.jpg', name: 'bouncy woods' , 'page': 3, 'id': 812 },
   { href: './html/blocky snakes/index.html', imgSrc: 'blocky snakes.jpg', name: 'blocky snakes' , 'page': 3, 'id': 809 },
   { href: './html/biker street/index.html', imgSrc: 'biker street.jpg', name: 'biker street' , 'page': 3, 'id': 807 },
