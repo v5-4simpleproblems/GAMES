6d686018a8e1d79ce4a03fe12e38701c7cbce3ec
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-12 13:35:37 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -6,7 +6,6 @@ export default [
       { href: './html/words search classic edition/index.html', imgSrc: 'word search classic edition.png', name: 'words search classic edition' , 'page': 3, 'id': 822 },
       { href: './html/wood blocks 3d/index.html', imgSrc: 'wood blocks 3d.jpg', name: 'wood blocks 3d' , 'page': 3, 'id': 821 },
       { href: './html/wizard mike/index.html', imgSrc: 'wizard mike.jpg', name: 'wizard mike' , 'page': 3, 'id': 820 },
-      { href: './html/where is my cat/index.html', imgSrc: 'where is my cat.jpg', name: 'where is my cat' , 'page': 3, 'id': 819 },
       { href: './html/boxing physics 2/index.html', imgSrc: 'boxing physics 2.jpg', name: 'boxing physics 2' , 'page': 3, 'id': 813 },
       { href: './html/boblehead soccer royale/index.html', imgSrc: 'bobblehead soccer royale.jpg', name: 'boblehead soccer royale' , 'page': 3, 'id': 811 },
       { href: './html/blumgi bloom/index.html', imgSrc: 'blumgi bloom.png', name: 'blumgi bloom' , 'page': 3, 'id': 810 },
