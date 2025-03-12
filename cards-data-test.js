3a4e4e5584e49ad0f66a5131e3a4922790122725
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-12 08:31:09 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -21,7 +21,6 @@ export default [
       { href: './html/blockpost/index.html', imgSrc: 'blockpost.jpg', name: 'blockpost' , 'page': 3, 'id': 808 },
       { href: './html/biker street/index.html', imgSrc: 'biker street.jpg', name: 'biker street' , 'page': 3, 'id': 807 },
       { href: './html/ball sort soccer/index.html', imgSrc: 'ball sort soccer.jpg', name: 'ball sort soccer' , 'page': 3, 'id': 806 },
-      { href: './html/baldis basics field trip/index.html', imgSrc: 'baldis basics field trip.jpg', name: 'baldis basics field trip' , 'page': 3, 'id': 805 },
       { href: './html/arcane archer/index.html', imgSrc: 'arcane archer.jpg', name: 'arcane archer' , 'page': 3, 'id': 803 },
       { href: './html/aquapark.io/index.html', imgSrc: 'aquapark.io.jpg', name: 'aquapark.io' , 'page': 3, 'id': 802 },
       { href: './html/apple shooter/index.html', imgSrc: 'apple shooter.jpg', name: 'apple shooter' , 'page': 3, 'id': 801 },
