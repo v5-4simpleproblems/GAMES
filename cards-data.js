fb4240260ab961f1b2bbd20e5f62fea30f52721b
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-13 12:56:24 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/stick defenders/index.html', imgSrc: 'stick defenders.jpg', name: 'stick defenders' , 'page': 3, 'id': 831 },
   { href: './html/stair race 3d/index.html', imgSrc: 'stair race 3d.jpg', name: 'stair race 3d' , 'page': 3, 'id': 830 },
   { href: './html/stacktris/index.html', imgSrc: 'stacktris.jpg', name: 'stacktris' , 'page': 3, 'id': 829 },
   { href: './html/squish run/index.html', imgSrc: 'squish run.png', name: 'squish run' , 'page': 3, 'id': 828 },
