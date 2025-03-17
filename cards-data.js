681e5e7b72a5b84f59eec45958b28e4321b54cbb
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-17 14:37:13 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/shoot stickman/index.html', imgSrc: 'shoot stickman.jpg', name: 'shoot stickman' , 'page': 3, 'id': 880 },
   { href: './html/rooftop snipers 2/index.html', imgSrc: 'rooftop snipers 2.jpg', name: 'rooftop snipers 2' , 'page': 3, 'id': 872 },
   { href: './html/scrap divers/index.html', imgSrc: 'scrap divers.jpg', name: 'scrap divers' , 'page': 3, 'id': 876 },
   { href: './html/rocket pult/index.html', imgSrc: 'rocket pult.jpg', name: 'rocket pult' , 'page': 3, 'id': 870 },
