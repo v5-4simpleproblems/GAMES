8b4a4f3b28a97bac23f6d5c16370c209b4318dc1
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-17 14:31:07 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/slime road/index.html', imgSrc: 'slime road.jpg', name: 'slime road' , 'page': 3, 'id': 889 },
   { href: './html/power badminton/index.html', imgSrc: 'power badminton.jpg', name: 'power badminton' , 'page': 3, 'id': 864 },
   { href: './html/runman race around the world/index.html', imgSrc: 'runman race around the world.png', name: 'runman race around the world' , 'page': 3, 'id': 873 },
   { href: './html/recoil/index.html', imgSrc: 'recoil.jpg', name: 'recoil' , 'page': 3, 'id': 868 },
