b49cbe8b9125453ba0ad9db1c86f5a48b0c037ea
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-18 09:09:00 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/hypersnake/index.html', imgSrc: 'hypersnake.jpg', name: 'hypersnake' , 'page': 3, 'id': 894 },
   { href: './html/dino bros/index.html', imgSrc: 'dino bros.jpg', name: 'dino bros' , 'page': 3, 'id': 923 },
   { href: './html/shoot stickman/index.html', imgSrc: 'shoot stickman.jpg', name: 'shoot stickman' , 'page': 3, 'id': 880 },
   { href: './html/rooftop snipers 2/index.html', imgSrc: 'rooftop snipers 2.jpg', name: 'rooftop snipers 2' , 'page': 3, 'id': 872 },
