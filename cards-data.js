61999702e11f7ada441793e9d41726e50a74c631
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-19 10:30:41 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/crazy tunnel 3d/index.html', imgSrc: 'crazy tunnel 3d.jpg', name: 'crazy tunnel 3d' , 'page': 4, 'id': 1035 },
   { href: './html/brain test tricky puzzles/index.html', imgSrc: 'brain test tricky puzzles.jpg', name: 'brain test tricky puzzles' , 'page': 4, 'id': 1004 },
   { href: './html/cubes king/index.html', imgSrc: 'cubes king.jpg', name: 'cubes king' , 'page': 4, 'id': 1037 },
   { href: './html/darts pro/index.html', imgSrc: 'darts pro.jpg', name: 'darts pro' , 'page': 4, 'id': 1042 },
