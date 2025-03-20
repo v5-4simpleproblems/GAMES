0347af46514c91aa03b4908d11eb2b5ac6630ce1
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-20 08:06:17 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -74,7 +74,6 @@ export default [
       { href: './html/extreme car parking/index.html', imgSrc: 'extreme car parking.jpg', name: 'extreme car parking' , 'page': 4, 'id': 940 },
       { href: './html/extreme car driving simulator/index.html', imgSrc: 'extreme car driving simulator.jpg', name: 'extreme car driving simulator' , 'page': 4, 'id': 939 },
       { href: './html/eugenes life/index.html', imgSrc: 'eugenes life.jpg', name: 'eugenes life' , 'page': 4, 'id': 938 },
-      { href: './html/energy/index.html', imgSrc: 'energy.jpg', name: 'energy' , 'page': 4, 'id': 937 },
       { href: './html/electron dash/index.html', imgSrc: 'electron dash.jpg', name: 'electron dash' , 'page': 4, 'id': 936 },
       { href: './html/eggbot vs zombies/index.html', imgSrc: 'eggbot vs zombies.jpg', name: 'eggbot vs zombies' , 'page': 4, 'id': 935 },
       { href: './html/duo survival 3/index.html', imgSrc: 'duo survival 3.jpg', name: 'duo survival 3' , 'page': 4, 'id': 933 },
