a6a3cf37ac0aff66f884a776d1c0fec926369f22
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-20 11:36:52 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/cricket world cup/index.html', imgSrc: 'cricket world cup.jpg', name: 'cricket world cup' , 'page': 4, 'id': 1036 },
   { href: './html/dark runner/index.html', imgSrc: 'dark runner.jpg', name: 'dark runner' , 'page': 4, 'id': 1041 },
   { href: './html/mafia wars/index.html', imgSrc: 'mafia wars.jpg', name: 'mafia wars' , 'page': 4, 'id': 904 },
   { href: './html/puppet master/index.html', imgSrc: 'puppet master.jpg', name: 'puppet master' , 'page': 3, 'id': 865 },
