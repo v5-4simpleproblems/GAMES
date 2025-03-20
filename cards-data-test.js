9ac65015f92ccd0ba0b3206a679e7d246dab22e9
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-20 08:20:16 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -121,7 +121,6 @@ export default [
       { href: './html/rio rex/index.html', imgSrc: 'rio rex.jpg', name: 'rio rex' , 'page': 3, 'id': 869 },
       { href: './html/real simulator monster truck/index.html', imgSrc: 'real simulator monster truck.jpg', name: 'real simulator monster truck' , 'page': 3, 'id': 867 },
       { href: './html/rally champion/index.html', imgSrc: 'rally champion.jpg', name: 'rally champion' , 'page': 3, 'id': 866 },
-      { href: './html/puppet master/index.html', imgSrc: 'puppet master.jpg', name: 'puppet master' , 'page': 3, 'id': 865 },
       { href: './html/poor bunny/index.html', imgSrc: 'poor bunny.jpg', name: 'poor bunny' , 'page': 3, 'id': 863 },
       { href: './html/truck traffic/index.html', imgSrc: 'truck traffic.jpg', name: 'truck traffic' , 'page': 3, 'id': 859 },
       { href: './html/striker dummies/index.html', imgSrc: 'striker dummies.jpg', name: 'striker dummies' , 'page': 3, 'id': 841 },
