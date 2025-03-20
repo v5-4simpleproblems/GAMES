8731558e68c683d029601a4af32b39821389ae9e
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-20 12:38:43 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/bacon may die/index.html', imgSrc: 'bacon may die.jpg', name: 'bacon may die' , 'page': 4, 'id': 972 },
   { href: './html/cow bay/index.html', imgSrc: 'cow bay.jpg', name: 'cow bay' , 'page': 4, 'id': 1033 },
   { href: './html/cricket world cup/index.html', imgSrc: 'cricket world cup.jpg', name: 'cricket world cup' , 'page': 4, 'id': 1036 },
   { href: './html/dark runner/index.html', imgSrc: 'dark runner.jpg', name: 'dark runner' , 'page': 4, 'id': 1041 },
