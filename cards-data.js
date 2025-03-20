ac1da2a30e16b0efc818f169e637bcfe325e9868
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-20 08:12:06 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/noob hook/index.html', imgSrc: 'noob hook.jpg', name: 'noob hook' , 'page': 4, 'id': 911 },
   { href: './html/eugenes life/index.html', imgSrc: 'eugenes life.jpg', name: 'eugenes life' , 'page': 4, 'id': 938 },
   { href: './html/energy/index.html', imgSrc: 'energy.jpg', name: 'energy' , 'page': 4, 'id': 937 },
   { href: './html/minesweeper/index.html', imgSrc: 'minesweeper.png', name: 'minesweeper' , 'page': 4, 'id': 907 },
