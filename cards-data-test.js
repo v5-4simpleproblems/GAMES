46801361d276dd52bbd978710a13ac5e39c70437
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-21 09:47:59 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -82,7 +82,6 @@ export default [
       { href: './html/doodle champion island/index.html', imgSrc: 'doodle champion island.jpg', name: 'doodle champion island' , 'page': 4, 'id': 924 },
       { href: './html/plactions/index.html', imgSrc: 'plactions.png', name: 'plactions' , 'page': 4, 'id': 920 },
       { href: './html/ping pong/index.html', imgSrc: 'ping pong.jpg', name: 'ping pong' , 'page': 4, 'id': 919 },
-      { href: './html/parkour race/index.html', imgSrc: 'parkour race.jpg', name: 'parkour race' , 'page': 4, 'id': 918 },
       { href: './html/paper fighter/index.html', imgSrc: 'paper fighter.jpg', name: 'paper fighter' , 'page': 4, 'id': 917 },
       { href: './html/pack a bag/index.html', imgSrc: 'pack a bag.jpg', name: 'pack a bag' , 'page': 4, 'id': 915 },
       { href: './html/orbital survival/index.html', imgSrc: 'orbital survival.jpg', name: 'orbital survival' , 'page': 4, 'id': 914 },
