d020c98c9802a61881f05a81fcd8553245e8a861
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-03-26 10:23:04 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/court kings 3d/index.html', imgSrc: 'court kings 3d.jpg', name: 'court kings 3d' , 'page': 4, 'id': 1032 },
   { href: './html/Patricks parabox/index.html', imgSrc: 'patricks parabox.jpg', name: 'Patricks parabox' , 'page': 4, 'id': 1081 },
   { href: './html/brain test 3 tricky quests/index.html', imgSrc: 'brain test 3 tricky quests.png', name: 'brain test 3 tricky quests' , 'page': 4, 'id': 1003 },
   { href: './html/cave blast/index.html', imgSrc: 'cave blast.jpg', name: 'cave blast' , 'page': 4, 'id': 1024 },
