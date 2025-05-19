4273e55005259b955536b1f2e981cb1cd6a1e554
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-19 17:22:28 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/3d moto simulator 2/index.html', imgSrc: '3d moto simulator 2.jpg', name: '3d moto simulator 2' , 'page': 4, 'id': 1046 },
   { href: './html/4x4 drive offroad/index.html', imgSrc: '4x4 drive offroad.jpg', name: '4x4 drive offroad' , 'page': 4, 'id': 1047 },
   { href: './html/running fred/index.html', imgSrc: 'running fred.jpg', name: 'running fred' , 'page': 3, 'id': 874 },
   { href: './html/block the pig/index.html', imgSrc: 'block the pig.jpg', name: 'block the pig' , 'page': 4, 'id': 998 },
