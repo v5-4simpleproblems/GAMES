4435b40bc4a45855bac977cf190a71a8359f70a8
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-19 16:53:19 -0600

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -91,7 +91,6 @@ export default [
       { href: './html/brutalmania.io/index.html', imgSrc: 'brutalmania.io.jpg', name: 'brutalmania.io' , 'page': 4, 'id': 1005 },
       { href: './html/bouncy basketball/index.html', imgSrc: 'bouncy basketball.jpg', name: 'bouncy basketball' , 'page': 4, 'id': 1001 },
       { href: './html/blocky cars/index.html', imgSrc: 'blocky cars.jpg', name: 'blocky cars' , 'page': 4, 'id': 999 },
-      { href: './html/block the pig/index.html', imgSrc: 'block the pig.jpg', name: 'block the pig' , 'page': 4, 'id': 998 },
       { href: './html/bladers/index.html', imgSrc: 'bladers.jpg', name: 'bladers' , 'page': 4, 'id': 996 },
       { href: './html/bike trials winter 2/index.html', imgSrc: 'bike trials winter 2.jpg', name: 'bike trials winter 2' , 'page': 4, 'id': 995 },
       { href: './html/bike trials winter 1/index.html', imgSrc: 'bike trials winter 1.jpg', name: 'bike trials winter 1' , 'page': 4, 'id': 994 },
