d16602818a2a2de6b1eef3880ba8365701380a94
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-19 11:17:50 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/a pretty odd bunny/index.html', imgSrc: 'a pretty odd bunny.jpg', name: 'a pretty odd bunny' , 'page': 4, 'id': 1049 },
   { href: './html/cat trap/index.html', imgSrc: 'cat trap.png', name: 'cat trap' , 'page': 4, 'id': 1021 },
   { href: './html/onion boy/index.html', imgSrc: 'onion boy.jpg', name: 'onion boy' , 'page': 4, 'id': 913 },
   { href: './html/animal arena/index.html', imgSrc: 'animal arena.png', name: 'animal arena' , 'page': 4, 'id': 1062 },
