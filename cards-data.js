16844c5a5b2c36450db4fab1a7cb99662cf32b5b
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-19 16:53:20 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/block the pig/index.html', imgSrc: 'block the pig.jpg', name: 'block the pig' , 'page': 4, 'id': 998 },
   { href: './html/ballhit.io/index.html', imgSrc: 'ballhit.io.jpg', name: 'ballhit.io' , 'page': 4, 'id': 974 },
   { href: './html/roly poly monsters/index.html', imgSrc: 'roly poly monsters.jpg', name: 'roly poly monsters' , 'page': 3, 'id': 871 },
   { href: './html/dreadhead parkour/index.html', imgSrc: 'dreadhead parkour.jpg', name: 'dreadhead parkour' , 'page': 4, 'id': 925 },
