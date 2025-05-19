6ffca7cfcffae0bbdaf68fcdfd1afd6de91f180d
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-19 13:17:02 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/kart race/index.html', imgSrc: 'kart race.jpg', name: 'kart race' , 'page': 4, 'id': 900 },
   { href: './html/age of war 2/index.html', imgSrc: 'age of war 2.jpg', name: 'age of war 2' , 'page': 4, 'id': 1053 },
   { href: './html/aliens nest/index.html', imgSrc: 'aliens nest.jpg', name: 'aliens nest' , 'page': 4, 'id': 1058 },
   { href: './html/arithmetica/index.html', imgSrc: 'arithmetica.jpg', name: 'arithmetica' , 'page': 4, 'id': 1072 },
