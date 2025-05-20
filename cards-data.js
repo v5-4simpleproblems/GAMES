e47ddcf410e6b6c68838dd63ee5fdb0f58d8be74
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-20 12:10:21 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/car rush/index.html', imgSrc: 'car rush.jpg', name: 'car rush' , 'page': 4, 'id': 1016 },
   { href: './html/big shot boxing/index.html', imgSrc: 'big shot boxing.jpg', name: 'big shot boxing' , 'page': 4, 'id': 990 },
   { href: './html/cars thief/index.html', imgSrc: 'cars thief.jpg', name: 'cars thief' , 'page': 4, 'id': 1018 },
   { href: './html/dunk perfect/index.html', imgSrc: 'dunk perfect.jpg', name: 'dunk perfect' , 'page': 4, 'id': 930 },
