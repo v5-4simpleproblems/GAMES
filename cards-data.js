d7f4002d77453ca5459ba4ea5d2f97dbd82b6f49
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-20 09:14:56 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/burnout drift seaport max/index.html', imgSrc: 'burnout drift seaport max.jpg', name: 'burnout drift seaport max' , 'page': 4, 'id': 1010 },
   { href: './html/cars thief tank edition/index.html', imgSrc: 'cars thief tank edition.jpg', name: 'cars thief tank edition' , 'page': 4, 'id': 1019 },
   { href: './html/sling drift/index.html', imgSrc: 'sling drift.jpg', name: 'sling drift' , 'page': 3, 'id': 890 },
   { href: './html/bearsus/index.html', imgSrc: 'bearsus.jpg', name: 'bearsus' , 'page': 4, 'id': 987 },
