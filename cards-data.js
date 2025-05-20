00783b6d3b3a4f43cd5ad92f8c4a2ac0f7dc9064
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-20 10:28:43 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/pack a bag/index.html', imgSrc: 'pack a bag.jpg', name: 'pack a bag' , 'page': 4, 'id': 915 },
   { href: './html/bouncy basketball/index.html', imgSrc: 'bouncy basketball.jpg', name: 'bouncy basketball' , 'page': 4, 'id': 1001 },
   { href: './html/burnout drift seaport max/index.html', imgSrc: 'burnout drift seaport max.jpg', name: 'burnout drift seaport max' , 'page': 4, 'id': 1010 },
   { href: './html/cars thief tank edition/index.html', imgSrc: 'cars thief tank edition.jpg', name: 'cars thief tank edition' , 'page': 4, 'id': 1019 },
