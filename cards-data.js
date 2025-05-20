b15e55b4112bfee0a4873dcf72d7ec08eac04276
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-20 12:16:57 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/burnin rubber crash n burn/index.html', imgSrc: 'burnin rubber crash n burn.jpg', name: 'burnin rubber crash n burn' , 'page': 4, 'id': 1009 },
   { href: './html/burnout extreme drift 2/index.html', imgSrc: 'burnout extreme drift 2.jpg', name: 'burnout extreme drift 2' , 'page': 4, 'id': 1011 },
   { href: './html/car rush/index.html', imgSrc: 'car rush.jpg', name: 'car rush' , 'page': 4, 'id': 1016 },
   { href: './html/big shot boxing/index.html', imgSrc: 'big shot boxing.jpg', name: 'big shot boxing' , 'page': 4, 'id': 990 },
