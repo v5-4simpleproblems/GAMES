22c6259086fe17be60ab8fbc3a0c6e926b029e1e
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-05-20 11:28:09 -0600

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/bad dolls/index.html', imgSrc: 'bad dolls.jpg', name: 'bad dolls' , 'page': 4, 'id': 973 },
   { href: './html/marble dash/index.html', imgSrc: 'marble dash.jpg', name: 'marble dash' , 'page': 4, 'id': 905 },
   { href: './html/mob city/index.html', imgSrc: 'mob city.jpg', name: 'mob city' , 'page': 4, 'id': 908 },
   { href: './html/mystical birdlink/index.html', imgSrc: 'mystical birdlink.jpg', name: 'mystical birdlink' , 'page': 4, 'id': 909 },
